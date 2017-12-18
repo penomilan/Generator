fea94a0c3cdd78096f9a0f5c1086f4bba8442466
MilanPeno <MilanPeno@MilanPeno.local>
2017-12-19 00:04:11 +0100

    css edited

--- js/generator.js
+++ js/generator.js
@@ -27,13 +27,18 @@ const inputTitle = document.getElementById("inputTitle");
         <html>
           <head>
             <title>$TITLE$</title>
-            <style>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+         <style>
             body {background-color: $COLOR$;}
             p { text-align: center;}
             h1 { text-align: center;}
             img { display: block; margin: auto; }
-            </style>
-          </head>
+         </style>
+         </head>
         <body>
           <div>
             <h1>$HEADING$</h1>
